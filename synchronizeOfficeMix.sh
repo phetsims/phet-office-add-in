2c90b90b066691304380d85bf2ae3db099899eef
mattpen <matthew.pennington@colorado.edu>
2019-01-09 12:48:18 -0700

    added labs api and updated files with repo name

--- synchronizeOfficeMix.sh
+++ synchronizeOfficeMix.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 # Copyright 2017, University of Colorado Boulder
 
-echo "Synchronizing phet-office-mix"
+echo "Synchronizing phet-office-add-in"
 date 
 
-cd /data/share/phet/phet-office-mix
+cd /data/share/phet/phet-office-add-in
 
 # Rewrite the sim array
 node updateSimArray.js
@@ -15,4 +15,4 @@ git pull
 git push
 
 # deploy it to /office-mix on the website
-cp -r public/* /data/web/static/phetsims/office-mix
+cp -r public/* /data/web/static/phetsims/office-add-in
